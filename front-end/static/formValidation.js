6d04cc2596ad7c1ec0d0ad36feca4c0c917b0109
lorenzo.gao <lorenzo.gao@ug.uchile.cl>
2024-10-08 20:48:16 +0800

    feat(formValidation.js): has finished the part of .js, wait for check.

--- front-end/static/formValidation.js
+++ front-end/static/formValidation.js
@@ -2,7 +2,7 @@ const validateReason = (reason) => {
     if (!reason) return true;
     else {
         // validación de longitud
-        let lengthValid = reason.trim().length <= 300;
+        let lengthValid = (reason.trim().length <= 300) && (reason.trim().length >= 3);
         return lengthValid;
     };
 };
@@ -10,22 +10,30 @@ const validateReason = (reason) => {
 const validateRadio = (radioName) => {
     const radios = document.getElementsByName(radioName);
     for (let radio of radios){
-        if (radio.checked){
-            return true;
-        };
+        // if (radio.checked){
+        //     return true;
+        if (radio.checked) return true;
     };
     return false;
 };
+const validateCalificationComment = (comment) => {
+    if (reasonBlock.hidden) return true;
+    if (comment.trim().length <=3) return false;
+    // if (!(comment.length >=3)) {
+    //     return false;
+    // }
+    return true;
+}
 
-const reasonInput = document.getElementById("professorCalificationReason");
+const reasonBlock = document.getElementById("professorCalificationReason");
 
 function hideReasonInput() {
-    reasonInput.hidden = true;
+    reasonBlock.hidden = true;
     return;
 };
 
 function unhideReasonInput() {
-    reasonInput.hidden = false;
+    reasonBlock.hidden = false;
     return;
 };
 
@@ -33,6 +41,10 @@ const validateForm = () => {
     let theForm = document.forms["mainForm"];
     let classCalReason = theForm["calificationReason"].value;
     let feedback = theForm["necessityFeedback"].value;
+    // let classCal = theForm["classCalification"].value;
+    // let professorCal = theForm["professorCalification"].value;
+    let hiddenInput = document.getElementById("professorCalReason");
+    
     // let 
     
     let invalidInputs = [];
@@ -42,11 +54,27 @@ const validateForm = () => {
         isValid &&= false;
     };
 
+    if (!validateCalificationComment(hiddenInput.value)){
+        setInvalidInput("hiddenInputArea");
+        // if(!(hiddenInput.value.trim().length >= 3)){
+        //     setInvalidInput("hiddenInputArea")
+        // }
+        // if (!validateReason(hiddenInput.value)) {
+        //     setInvalidInput("hiddenInputArea");
+        // };
+    };
+
     if (!validateReason(classCalReason)) {
         setInvalidInput("textArea");
     };
     if (!validateReason(feedback)) {
         setInvalidInput("textArea2");
+    };
+    if (!validateRadio("classCalification")) {
+        setInvalidInput("classCalification");
+    };
+    if (!validateRadio("professorCalification")) {
+        setInvalidInput("professorCalification");
     };
 
 
@@ -56,7 +84,7 @@ const validateForm = () => {
     
     if (!isValid) {
         notificationList.textContent = "";
-        for (input of invalidInputs) {
+        for (let input of invalidInputs) {
             let listElement = document.createElement("li");
             listElement.innerText = input;
             notificationList.append(listElement);
@@ -67,31 +95,32 @@ const validateForm = () => {
         notificationBox.hidden = false;
     } else {
         theForm.style.display = "none";
-        notificationMessage.innerText = "¿Confirma que desea publicar esta donación?";
+        notificationMessage.innerText = "¿Confirma que desea enviar esta retroalimentación?";
         notificationList.textContent = "";
+
+        notificationBox.style.backgroundColor = "#ddffdd";
+        notificationBox.style.borderLeftColor = "#4CAF50";
+    
+        let submitButton = document.createElement("button");
+        submitButton.innerText = "Sí, confirmo";
+        submitButton.style.marginRight = "10px";
+    
+        let backButton = document.createElement("button");
+        backButton.innerText = "No, quiero volver al formulario";
+        backButton.addEventListener("click", () => {
+            theForm.style.display = "block";
+            notificationBox.hidden = true;
+        });
+        submitButton.addEventListener("click", () => {
+            notificationMessage.innerText = "Hemos recibido su retroalimentación. Muchas gracias."
+            submitButton.hidden = true;
+            backButton.hidden = true;
+            //theForm.submit();
+        })
+        notificationList.appendChild(submitButton);
+        notificationList.appendChild(backButton);
+        notificationBox.hidden = false;
     };
-    notificationBox.style.backgroundColor = "#ddffdd";
-    notificationBox.style.borderLeftColor = "#4CAF50";
-
-    let submitButton = document.createElement("button");
-    submitButton.innerText = "Sí, confirmo";
-    submitButton.style.marginRight = "10px";
-
-    let backButton = document.createElement("button");
-    backButton.innerText = "No, quiero volver al formualrio";
-    backButton.addEventListener("click", () => {
-        theForm.style.display = "block";
-        notificationBox.hidden = true;
-    });
-    submitButton.addEventListener("click", () => {
-        notificationMessage.innerText = "Hemos recibido su retroalimentación. Muchas gracias."
-        submitButton.hidden = true;
-        backButton.hidden = true;
-        //theForm.submit();
-    })
-    notificationList.appendChild(submitButton);
-    notificationList.appendChild(backButton);
-    notificationBox.hidden = false;
 };
 
 
