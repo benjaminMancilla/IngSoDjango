48350f1159b7403629972c59e85b41958849a96d
Benjamin Mancilla <benjamin.mancilla@ug.uchile.cl>
2024-10-12 17:59:42 -0300

    FIX(urls.py): deleted default django view for login

--- feedback_app/urls.py
+++ feedback_app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.root_redirect, name='root'),
-    path('login/', auth_views.LoginView.as_view(template_name='feedback_app/login.html'), name='login'),
+    path('login/', views.login_view, name='login'),
     path('home-page/', views.homepage, name='home-page'),
     path('form/', views.form, name='form'),
 ]
