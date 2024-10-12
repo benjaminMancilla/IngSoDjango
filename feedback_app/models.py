f4252f0680e2412e93139c219a15204ec9b54128
unich <gabriel.gallardo@ug.uchile.cl>
2024-10-12 14:31:06 -0300

    FEAT(Models): Adding models to django
    The models include a new User for django, that is added to the settings
    in order to make the app work with it.

---
+++ feedback_app/models.py
@@ -0,0 +1,60 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+
+    STUDENT = 1
+    TEACHER = 2
+    ROLES = (
+        (STUDENT, 'student'),
+        (TEACHER, 'teacher')
+    )
+
+    role = models.PositiveSmallIntegerField(choices=ROLES)
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    mask = models.CharField(max_length=100)
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+
+class SubjectResume(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    resume = models.CharField(max_length=500)
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(fields=("teacher", "subject", "date"), name='unique_teacher_subject_date'),
+        )
+
+
+class TeacherStudentSubject(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(fields=("teacher", "student", "subject"), name='unique_teacher_student_subject'),
+        )
+
+class Feedback(models.Model):
+    tss = models.ForeignKey(TeacherStudentSubject, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    grade = models.PositiveSmallIntegerField()
+    content = models.CharField(max_length=300)
+    
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(fields=("tss", "date"), name='unique_tss_date'),
+        )
+
+class Question(models.Model):
+    tss = models.ForeignKey(TeacherStudentSubject, on_delete=models.CASCADE)
+    content = models.CharField(max_length=300)
