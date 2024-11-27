1fcf3b58874b2f418d2c299874866293c60e18c0
Benjamin Mancilla <benjamin.mancilla@ug.uchile.cl>
2024-11-27 05:15:45 -0300

    FEAT(urls): adding url domain in foro for week number

--- feedback_app/urls.py
+++ feedback_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('home-page/', views.homepage, name='home-page'),
     path('foro/<int:teacherId>/<int:subjectId>/', views.foro, name='foro'),
+    path('foro/<int:teacherId>/<int:subjectId>/<int:week_n>', views.foro, name='foro'),
     path('form/', views.form, name='form'),
     path('logout/', views.logout_view, name='logout'),
     path('add-week/<int:teacherId>/<int:subjectId>/', views.add_week, name='add_week'),
